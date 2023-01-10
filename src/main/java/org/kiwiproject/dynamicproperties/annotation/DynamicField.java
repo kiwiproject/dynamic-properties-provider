568a410931434ad4d1577455681d6b403a32de2b
David Singley <dsingley@vt.edu>
2023-01-10 14:40:43 -0500

    Apply suggestions from code review
    Co-authored-by: Scott Leberknight <174812+sleberknight@users.noreply.github.com>

--- src/main/java/org/kiwiproject/dynamicproperties/annotation/DynamicField.java
+++ src/main/java/org/kiwiproject/dynamicproperties/annotation/DynamicField.java
@@ -47,7 +47,7 @@ public @interface DynamicField {
     /**
      * Provide a list of possible values for this field. Useful if field is restricted to a set of values.
      *
-     * <b>Note:</b> Only one attribute providing choices may be specified per field.
+     * <em>Note:</em> Only one attribute providing choices may be specified per field.
      *
      * @return the choices that should be available for this field, default is empty array
      */
@@ -57,17 +57,17 @@ public @interface DynamicField {
      * Provide a list of possible values for this field by using the given Enum as the list. Useful if field is
      * restricted to a set of values.
      *
-     *  <b>Note:</b> Only one attribute providing choices may be specified per field.
+     *  <em>Note:</em> Only one attribute providing choices may be specified per field.
      *
      * @return the choices that should be available for this field based on the given Enum.
      */
-    Class<? extends Enum> choicesFromEnum() default NullEnum.class;
+    Class<? extends Enum<?> choicesFromEnum() default NullEnum.class;
 
     /**
      * Provide a list of possible values with labels and enabled-ness for this field by using the given supplier.
      * Useful if field is restricted to a set of values.
      *
-     * <b>Note:</b> Only one attribute providing choices may be specified per field.
+     * <em>Note:</em> Only one attribute providing choices may be specified per field.
      *
      * @return the {@link Choice}s that should be available for this field.
      */
